3dbd07dcabfa05c8f627d402906ca10af01f3969
Haris Khan <hariskhan01.gsm@gmail.com>
2025-06-25 18:56:51 +0500

    project Quickblog

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
+import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
+
+
+
+const app = express();
+
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => res.send("API is working"));
+app.use("/api/admin", adminRouter);
+app.use("/api/blog", blogRouter);
+
+const PORT = process.env.PORT || 3000;
+const MONGOURL = process.env.MONGO_URL
+
+mongoose.connect(MONGOURL)
+    .then(() => {
+    console.log("Database connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }).catch((error)=> console.log(error))
+
+export default app;
